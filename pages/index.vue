4ca5eb594a49c563ff79beb53dd8ca37ea661ea0
zitherSide <takuyakotoyori@akane.waseda.jp>
2021-01-03 08:06:59 +0900

    created plot page.

--- pages/index.vue
+++ pages/index.vue
@@ -1,79 +1,23 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <v-main>
+    <v-row v-for="(row, rowId) in cards" :key="rowId" align="center" justify="center">
+      <v-col v-for="(card, colId) in row" :key="colId" :cols="Math.max(1, card.cols)">
+        <v-card >
+          <v-card-actions class='justify-space-between'>
+            <v-btn icon v-if="card.cols > 0"
+              @click="cards[rowId][colId].cols-=1"><v-icon>mdi-arrow-collapse</v-icon></v-btn>
+            <v-btn icon 
+              @click="cards[rowId][colId].cols+=1"><v-icon>mdi-arrow-expand</v-icon></v-btn>
+          </v-card-actions>
+          <v-card-text v-if="card.cols > 0">
+            <v-textarea auto-grow v-model="card.line" dense flat/>
+            <v-textarea auto-grow v-model="card.situation" flat/>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-btn outlined><v-icon>mdi-plus</v-icon></v-btn>
+    </v-row>
+  </v-main>
 </template>
 
 <script>
@@ -84,6 +28,63 @@ export default {
   components: {
     Logo,
     VuetifyLogo
+  },
+  data() {
+    return {
+      cards: [
+        [
+          {
+            line: 'Hey!',
+            situation: 'Noticed',
+            cols: 3
+          },
+          {
+            line: 'Hey!',
+            situation: 'Noticed',
+            cols: 4
+          },
+          {
+            line: 'Hey!',
+            situation: 'Noticed',
+            cols: 4
+          }
+        ],
+        [
+          {
+            line: 'Hey!',
+            situation: 'Noticed',
+            cols: 3
+          },
+          {
+            line: 'Hey!',
+            situation: 'Noticed',
+            cols: 4
+          },
+          {
+            line: 'Hey!',
+            situation: 'Noticed',
+            cols: 4
+          }
+        ],
+        [
+          {
+            line: 'Hey!',
+            situation: 'Noticed',
+            cols: 3
+          },
+          {
+            line: 'Hey!',
+            situation: 'Noticed',
+            cols: 4
+          },
+          {
+            line: 'Hey!',
+            situation: 'Noticed',
+            cols: 4
+          },
+        ]
+      ]
+    }
   }
 }
 </script>
